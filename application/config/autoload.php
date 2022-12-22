044889beb591bb45642e513e9b2350ace2aa3571
Asep supian tsaori <asep.tsaori07@gmail.com>
2022-12-23 03:24:28 +0700

    helper error

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','file','template', 'userAccess', 'security', 'form');
+$autoload['helper'] = array('url','file','template', 'useraccess', 'security', 'form');
 
 /*
 | -------------------------------------------------------------------
