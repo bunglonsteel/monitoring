3fb26e573477d5c2a467133bc1a67671b677201b
Asep supian tsaori <asep.tsaori07@gmail.com>
2023-01-04 19:46:37 +0700

    add send message

---
+++ application/helpers/send_message_helper.php
@@ -0,0 +1,34 @@
+<?php
+
+if (!function_exists('send_message_whatsapp')) {
+
+	function send_message_whatsapp($notlp, $message) {
+		$token = 'IhJ5FBjMjVarKakW@+t0';
+		
+		$curl = curl_init();
+		$config = [
+			CURLOPT_URL => 'https://api.fonnte.com/send',
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => '',
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => 'POST',
+			CURLOPT_POSTFIELDS => [
+				'target' => "$notlp",
+				'message' => "$message", 
+				'countryCode' => '62', //optional
+			],
+			CURLOPT_HTTPHEADER => [
+				"Authorization: $token" //change TOKEN to your actual token
+			],
+		];
+
+		curl_setopt_array($curl, $config);
+
+		$response = curl_exec($curl);
+		curl_close($curl);
+		return $response;
+	}
+}
